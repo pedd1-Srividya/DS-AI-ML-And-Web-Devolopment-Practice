aae00bb604565acce59fef6bc7313b3957244966
Peddi Srividya <srividyapeddi04@gmail.com>
2025-11-25 14:25:27 +0530

    Add files via upload

---
+++ Full Stack/javascript/first.js
@@ -0,0 +1,38 @@
+console.log("Hello")
+//variables
+let age=25;
+age=30;
+console.log(age)
+const pi=3.14;
+//pi=4;
+console.log(pi)
+var name='Alice';
+name='charlie';
+console.log(name);
+//datatypes
+let a="String";//string
+let b=25;//number
+let c=true//boolean
+let d;//undefined
+let e=null;//null
+let f=465949461545849889846164648764944946//big int
+//operators
+//arithmetic operator
+let x=10;
+let y=5;
+console.log(x+y)
+console.log(x-y)
+console.log(x*y)
+console.log(x/y)
+console.log(x%y)
+//assignment operator
+a=10;
+a+=5;
+console.log(a)
+//comparision operator
+console.log(10=='10')
+console.log(10==='10')
+console.log(10<20)
+//logical operator
+console.log(10<20 && 20<10)
+console.log(10<20 || 20<10)
