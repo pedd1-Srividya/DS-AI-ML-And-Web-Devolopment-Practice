6c58731bfe3472c66e373e593201b71faf662470
Peddi Srividya <srividyapeddi04@gmail.com>
2025-12-02 00:55:10 +0530

    Add files via upload

---
+++ Full Stack/js-html/object_function.js
@@ -0,0 +1,34 @@
+let names=['akjshu','ghsu','ushubh']
+for ( let item of names)
+{
+    console.log(item)
+}
+names.forEach(f=>console.log(f))
+let person={
+    name:"jwijw",
+    age:15,
+    isStudent:true,
+    list:[15,65,75]
+}
+console.log(person)
+console.log(person["age"])
+console.log(person["list"][0])
+person.city="Delhi"
+console.log(person["city"])
+person["Country"]="India"
+console.log(person["Country"])
+
+
+for(let key in person)
+{
+    console.log(key,":",person[key])
+}
+
+
+
+
+
+
+
+
+
